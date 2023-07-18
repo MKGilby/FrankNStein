aa01e71da21032bda3a938c039a44e0206d9241c
HUNGilby <gilby@mksztsz.hu>
2023-07-18 20:56:31 +0200

    Update build_all.bat

--- work/build_all.bat
+++ work/build_all.bat
@@ -1,8 +1,12 @@
 @echo off
+if not exist ..\data\ (mkdir ..\data)
+if not exist ..\data\decorations\ (mkdir ..\data\decorations)
 cd decorations
 call run.bat
 cd ..\maps
 call run.bat
 cd ..\fonts
+call run.bat
+cd ..\logo
 call run.bat
 cd ..
