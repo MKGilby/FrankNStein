5de52bbdb72c9f6ebadc9226cc6a0de8dfc541a4
HUNGilby <gilby@mksztsz.hu>
2023-07-21 21:30:28 +0200

    Change in font
    - Added a few chars

--- work/Fonts/run.bat
+++ work/Fonts/run.bat
@@ -2,7 +2,7 @@
 echo This script creates font file from font image.
 ..\..\tools\fontbuild2 data\font.png fontface.png -charset "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@-.!,()'" -colorkey -sort
 TextureFont
-..\..\tools\fontbuild2 data\cs_font.bdc cs_font.png -charset "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-@./!'" -colorkey -sort
+..\..\tools\fontbuild2 data\cs_font.bdc cs_font.png -charset "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-@.#2f',:" -colorkey
 ..\..\tools\pngout font.png ..\..\data\font.png /Y /kanMZ,fnTZ,anIM
 ..\..\tools\pngout cs_font.png ..\..\data\cs_font.png /Y /kanMZ,fnTZ,anIM
 del fontface.png
