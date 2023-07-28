13f68d6ed0e16ff96ca091a8168893d7347a6ded
HUNGilby <gilby@mksztsz.hu>
2023-07-28 15:21:13 +0200

    Added some gfx...

--- work/Decorations/run.bat
+++ work/Decorations/run.bat
@@ -2,5 +2,6 @@
 echo This script converts the old format graphics to PNGs.
 
 ..\..\tools\mkconv2 convert.mc2
-for %%i in (*.png) do ..\..\tools\pngout %%i ..\..\data\decorations\%%i /y /kanMZ,fnTZ,anIM /f0
+if not exist ..\..\data\ (mkdir ..\..\data)
+for %%i in (*.png) do ..\..\tools\pngout %%i ..\..\data\%%i /y /kanMZ,fnTZ,anIM /f0
 del *.png
