2530ba98a5dceb2c1eeeffd0deb44f23c709f8e2
HUNGilby <gilby@mksztsz.hu>
2023-11-29 14:41:43 +0100

    Updating font
    - Added A, B, left and right controller buttons as characters (128..131)

--- work/Fonts/run.bat
+++ work/Fonts/run.bat
@@ -1,10 +1,5 @@
 @echo off
 echo This script creates font file from font image.
-rem ..\..\tools\fontbuild2 data\font.png fontface.png -charset "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@-.!,()'" -colorkey -sort
-rem TextureFont
-..\..\tools\fontbuild2 data\cs_font.bdc cs_font.png -charset "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-@.#2f',:!?" -colorkey
-rem ..\..\tools\pngout font.png ..\..\data\font.png /Y /kanMZ,fnTZ,anIM
+..\..\tools\fontbuild2 data\cs_font.png cs_font.png -charset "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-@.#2f',:!?#80#81#82#83" -colorkey
 ..\..\tools\pngout cs_font.png ..\..\data\cs_font.png /Y /kanMZ,fnTZ,anIM
-rem del fontface.png
-rem del font.png
 del cs_font.png
