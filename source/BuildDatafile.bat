d81733e097fcda881fb121be10f07d705d62961e
MKGilby <gilby@mksztsz.hu>
2025-09-02 14:40:04 +0200

    Updated build scripts

--- source/BuildDatafile.bat
+++ source/BuildDatafile.bat
@@ -1,3 +1,7 @@
-if exist ..\_release\FrankNStein.data del ..\_release\FrankNStein.data
-..\tools\mad4 ..\_release\FrankNStein.data ..\data * -1 -r
-
+call ..\work\setenv.bat
+if not exist ..\release\ (mkdir ..\release)
+if not exist ..\release\x64 (mkdir ..\release\x64)
+if not exist ..\release\x86 (mkdir ..\release\x86)
+if exist ..\release\x64\FrankNStein.data del ..\release\x64\FrankNStein.data
+%SOURCETOOLSDIR%\mad4 ..\release\x64\FrankNStein.data ..\data * -1 -r -n
+copy /y ..\release\x64\FrankNStein.data ..\release\x86
--- source/BuildRelease_x64.bat
+++ source/BuildRelease_x64.bat
@@ -1,4 +1,7 @@
 @echo off
-if not exist lib\ (mkdir lib)
-if not exist lib\x86_64-win64\ (mkdir lib\x86_64-win64) else (del /Q lib\x86_64-win64\*)
-C:\lazarus\fpc\3.2.2\bin\x86_64-win64\fpc.exe -MDelphi -Scghi -CX -O3 -XX -l -vewnhibq -vm6058,5024 -Filib\x86_64-win64 -Fuunits -Fuunits\SDL2 -FuC:\lazarus\lcl\units\x86_64-win64\win32 -FuC:\lazarus\lcl\units\x86_64-win64 -FuC:\lazarus\components\freetype\lib\x86_64-win64 -FuC:\lazarus\components\lazutils\lib\x86_64-win64 -FuC:\lazarus\packager\units\x86_64-win64 -Fu. -FUlib\x86_64-win64 -FE..\_release -oFrankNStein_x64.exe FrankNStein.lpr
+call ..\work\setenv.bat
+if not exist ..\release\ (mkdir ..\release)
+if not exist ..\release\x64 (mkdir ..\release\x64)
+if not exist lib\ (mkdir lib) else (del /Q lib\*)
+%FPCDIR%\fpc.exe -Twin64 -Px86_64 -l- -MDelphi -Scghi -CX -O3 -XX -v0 -vm6058,5024 -Filib -Fuunits -Fuunits\SDL2 -Fu. -FUlib -FE..\release\x64 -oFrankNStein.exe FrankNStein.lpr
+RMDIR "lib" /S /Q
