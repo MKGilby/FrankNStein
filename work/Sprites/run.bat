959c23c478d8fdc4d22a8708f29ebbe9af09339f
MKGilby <gilby@mksztsz.hu>
2025-09-26 09:19:37 +0200

    Added meter font, updated scripts
    - Added electro-meter font for rebooted version.
    - Updated font and sprite scripts.

--- work/Sprites/run.bat
+++ work/Sprites/run.bat
@@ -6,6 +6,6 @@ call ..\setenv.bat
 %WORKTOOLSDIR%\SkeletonPrep
 %WORKTOOLSDIR%\mkconv2 convert.mc2
 del skeletonsprite.png
-for %%i in (*.png) do %WORKTOOLSDIR%\pngout %%i ..\..\data\%%i /y /kanMZ,fnTZ,anIM /f0
+for %%i in (*.png) do %WORKTOOLSDIR%\pngout %%i %WORKDATADIR%\%%i /y /kanMZ,fnTZ,anIM /f0
 del *.png
 del *.log
