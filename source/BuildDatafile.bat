5c581d35a05b55a63915371a6a4dd1f7f7304d4d
MKGilby <gilby@mksztsz.hu>
2025-10-31 14:24:13 +0100

    Further update build scripts

--- source/BuildDatafile.bat
+++ source/BuildDatafile.bat
@@ -1,7 +1,10 @@
 call ..\work\setenv.bat
-if not exist ..\release\ (mkdir ..\release)
-if not exist ..\release\x64 (mkdir ..\release\x64)
-if not exist ..\release\x86 (mkdir ..\release\x86)
-if exist ..\release\x64\FrankNStein.data del ..\release\x64\FrankNStein.data
-%SOURCETOOLSDIR%\mad4 ..\release\x64\FrankNStein.data ..\data * -1 -r -n
-copy /y ..\release\x64\FrankNStein.data ..\release\x86
+if exist ..\release\x64 (
+  if exist ..\release\x64\FrankNStein.data del ..\release\x64\FrankNStein.data
+  %SOURCETOOLSDIR%\mad4 ..\release\x64\FrankNStein.data ..\data * -1 -r -n
+)
+
+if exist ..\release\x86 (
+  if exist ..\release\x86\FrankNStein.data del ..\release\x86\FrankNStein.data
+  %SOURCETOOLSDIR%\mad4 ..\release\x86\FrankNStein.data ..\data * -1 -r -n
+)
